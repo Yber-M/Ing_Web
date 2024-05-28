2b2a59e44c9eb1485c2ff41f9f366c5a027c4b3e
Yber-M <ybermarlon@gmail.com>
2024-05-27 19:21:28 -0500

    Semana 09

---
+++ Semana09/frmPerson.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Personal</title>
+</head>
+
+<body>
+
+    <h2>REGISTRO DE ESTUDIANTES</h2>
+
+    <form action="" method="post">
+
+        Nombres: <input type="text" name="nombres" id="nombres"> <br>
+
+        <br> Apellidos: <input type="text" name="apellidos" id="apellidos"> <br>
+
+        <br> Edad: <input type="text" name="edad" id="edad"> <br>
+
+        <br> Carrera <input type="text" name="carrera" id="carrera"> <br>
+
+        <br> <input type="submit" name="btn-registrar" value="Registrar">
+
+    </form>
+
+</body>
+
+</html>
