7aa9f9f3400bd8e24b9c12db7111a56b82ee3ea2
Yber-M <ybermarlon@gmail.com>
2024-05-27 19:50:27 -0500

    Se imprime los datos ingresados en el formulario

--- Semana09/Controllers/Persona.php
+++ Semana09/Controllers/Persona.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("./model/clsPersona.php");
+require_once("../Model/clsPersona.php");
 
 $name = $_POST["nombres"];
 $lastname = $_POST["apellidos"];
@@ -9,4 +9,8 @@ $carrer = $_POST["carrera"];
 
 $people = new Persona($name, $lastname, $age, $carrer);
 
-echo "<h2><h2>";
+echo "<h2>Registro del Alumno<h2>";
+echo "Nombres: " . $people->getNombre() . "<br>";
+echo "Apellidos: " . $people->getApellidos() . "<br>";
+echo "Edad: " . $people->getEdad() . "<br>";
+echo "Carrera: " . $people->getCarrera() . "<br>";
