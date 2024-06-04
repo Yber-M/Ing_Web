aedabde1b2168d413c89c8743bafa46bc19603de
Yber-M <ybermarlon@gmail.com>
2024-06-03 20:17:05 -0500

    Se corrigio los problemas

--- Semana08/index.php
+++ Semana08/index.php
@@ -3,8 +3,8 @@
 // ? 
 require_once("model/clsOperaciones.php");
 
-$num1 = 14;
-$num2 = 20;
+$num1 = $_POST["num_ejc1"];
+$num2 = $_POST["num_ejc2"];
 $espacio = "<br><br>";
 
 $Operaciones = new Operacion($num1, $num2);
@@ -17,14 +17,14 @@ echo $num1 . " + " . $num2 . " = " . $totalSum . $espacio;
 //* RESTAR(-)
 echo "<h2>RESTAR(-)</h2>";
 $totalResta = $Operaciones->getRestar();
-echo $num1 . " - " . $num2 . " = " . $totalSum . $espacio;
+echo $num1 . " - " . $num2 . " = " . $totalResta . $espacio;
 
 //*  MULTIPLICAR(*)
 echo "<h2>MULTIPLICAR(*)</h2>";
 $totalMulti = $Operaciones->getMultiplicar();
-echo $num1 . " * " . $num2 . " = " . $totalSum . $espacio;
+echo $num1 . " * " . $num2 . " = " . $totalMulti . $espacio;
 
 //*  DIVIDIR(/)
 echo "<h2>DIVIDIR(/)</h2>";
-$totalMulti = $Operaciones->getDividir();
-echo $num1 . " / " . $num2 . " = " . $totalSum . $espacio;
+$totalDividir = $Operaciones->getDividir();
+echo $num1 . " / " . $num2 . " = " . $totalDividir . $espacio;
